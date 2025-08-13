3f373ce24471c672b372287ebee94e0b17cd1959
giovannyjvr <giovannyjvrusso@gmail.com>
2025-08-13 10:39:43 -0300

    arrumei a entrada e testes manuais passando.

--- main.py
+++ main.py
@@ -1,56 +1,49 @@
-def main(string):
-    soma = 0
-    numero = ""
-    space = False
-    token = ""
-    operacao = "+"
+import sys
 
-    for s in string:
-        if s.isdigit() :
-            if space == True and token == "n":
-                return ValueError("Invalid expression")
-            numero += s
-            token = "n"
-            space = False
+string = sys.argv[1]
 
-        elif s == " ":
-            space = True
+soma = 0
+numero = ""
+space = False
+token = ""
+operacao = "+"
 
-        elif s in "+-":
-            if token != "s":
-                return ValueError("Invalid expression")
-            
-            if token == "" and s == "-":
-                numero = ""
-                operacao = s
-                token = "s"
-                space = False
-            
-            elif token == "" and s == "+":
-                return ValueError("Invalid expression")
-            
-            else:
-                if operacao == "+":
-                    soma += int(numero)
-                elif operacao == "-":
-                    soma -= int(numero)
-                numero = ""
-                operacao = s
-                token = "s"
-                space = False
+for s in string:
+    if s.isdigit() :
+        if space == True and token == "n":
+            raise Exception("Invalid expression 'num  space  num'")
+        numero += s
+        token = "n"
+        space = False
+
+    elif s == " ":
+        space = True
+
+    elif s in "+-":
+        if token == "s":
+            raise Exception("Invalid expression")
+
+        elif token == "":
+            raise Exception("Invalid expression")
 
         else:
-            try:
-                raise ValueError("Invalid character found")
-            except ValueError as e:
-                return(e)
+            if operacao == "+":
+                soma += int(numero)
+            elif operacao == "-":
+                soma -= int(numero)
+            numero = ""
+            operacao = s
+            token = "s"
+            space = False
 
-    if token =="s":
-        return ValueError("Invalid expression")
-    if operacao == "+":
-        soma += int(numero)
-    elif operacao == "-":
-        soma -= int(numero)
-        
-    return soma
+    else:
+        raise Exception("Invalid character found")
 
+if token =="s":
+    raise Exception("Invalid expression")
+if operacao == "+":
+    soma += int(numero)
+elif operacao == "-":
+    soma -= int(numero)
+    
+print(soma)
