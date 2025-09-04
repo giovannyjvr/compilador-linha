492ca649d6d8f4fb1c433a6f00625c43288209e9
giovannyjvr <giovannyjvrusso@gmail.com>
2025-09-04 19:50:21 -0300

    roteiro 4. testes passam, arvore AST implementada

--- main.py
+++ main.py
@@ -53,49 +53,87 @@ class Lexer:
             raise Exception(f"Caracter inválido: {self.source[self.position]}")
         
 
+class Node:
+    def __init__(self, value, children: list):
+        self.value = value
+        self.children = children
 
+    def evaluate(self):
+        """Método abstrato que deve ser sobrescrito nas subclasses."""
+        pass
+
+class BinOp(Node):
+          
+
+    def evaluate(self):
+        left_val = self.children[0].evaluate()
+        right_val = self.children[1].evaluate()
+
+        if self.value == "PLUS":
+            return left_val + right_val
+        elif self.value == "MINUS":
+            return left_val - right_val
+        elif self.value == "MULT":
+            return left_val * right_val
+        elif self.value == "DIV":
+            return left_val // right_val
+
+
+class UnOp(Node):
+    
+    def evaluate(self):
+        val = self.children[0].evaluate()
+
+        if self.value == "PLUS":
+            return val
+        elif self.value == "MINUS":
+            return -val
+
+class IntVal(Node):
+    def evaluate(self):
+        return self.value
 
 class Parser:
     def parse_expression() -> int:
-        resultado = Parser.parse_term()
+        node = Parser.parse_term()
         while Parser.lex.next.kind in ("PLUS", "MINUS"):
             if Parser.lex.next.kind == "PLUS":
                 Parser.lex.select_next()
-                resultado += Parser.parse_term()
+                node = BinOp("PLUS", [node, Parser.parse_term()])
             elif Parser.lex.next.kind == "MINUS":
                 Parser.lex.select_next()
-                resultado -= Parser.parse_term()
-        return resultado
+                node = BinOp("MINUS", [node, Parser.parse_term()])
+        return node
 
     def parse_term():
-        resultado = Parser.parse_factor()
+        node = Parser.parse_factor()
         while Parser.lex.next.kind in ("MULT", "DIV"):
             if Parser.lex.next.kind == "MULT":
                 Parser.lex.select_next()
-                resultado *= Parser.parse_factor()
+                node = BinOp("MULT", [node, Parser.parse_factor()])
             elif Parser.lex.next.kind == "DIV":
                 Parser.lex.select_next()
-                resultado //= Parser.parse_factor()
-        return resultado
+                node = BinOp("DIV", [node, Parser.parse_factor()])
+        return node
 
     def parse_factor():
         if Parser.lex.next.kind == "INT":
-            resultado = Parser.lex.next.value
+            node = IntVal(Parser.lex.next.value,[])
             Parser.lex.select_next()
-            return resultado
+            return node
         elif Parser.lex.next.kind == "OPEN_PAR":
             Parser.lex.select_next()
-            resultado = Parser.parse_expression()
+            node = Parser.parse_expression()
             if Parser.lex.next.kind != "CLOSE_PAR":
                 raise Exception("Faltando )")
             Parser.lex.select_next()
-            return resultado
+            return node
         elif Parser.lex.next.kind == "MINUS":
             Parser.lex.select_next()
-            return -Parser.parse_factor()
+            return UnOp("MINUS", [Parser.parse_factor()])
         elif Parser.lex.next.kind == "PLUS":
             Parser.lex.select_next()
-            return Parser.parse_factor()
+            return UnOp("PLUS", [Parser.parse_factor()])
         else:
             raise Exception("Fator inválido")
         
@@ -106,7 +144,7 @@ class Parser:
         resultado = Parser.parse_expression()
         if Parser.lex.next.kind != "EOF":
             raise Exception("Expressão inválidas,,")
-        return resultado
+        return resultado.evaluate()
     
 codigo= sys.argv[1]
 print(Parser.run(codigo))
